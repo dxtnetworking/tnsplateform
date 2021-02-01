f5a74a6577d14bc03d84fbf169c74f86ef2a1e1a
Demba Siby <dembasiby@gmail.com>
2021-02-01 11:13:57 +0000

    Ajout validations au modele Profile

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  
+  validates :niveau_etude, presence: true, allow_blank: false
+  validates :occupation_actuelle, presence: true, allow_blank: false
+  
 end
