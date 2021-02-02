c8a01062a21f310f889e29a3f1d1a57358f54df3
Demba Siby <dembasiby@gmail.com>
2021-02-02 21:42:55 +0000

    Mise a jour migration

--- backend/db/migrate/20210202212544_create_pays.rb
+++ backend/db/migrate/20210202212544_create_pays.rb
@@ -1,8 +1,8 @@
 class CreatePays < ActiveRecord::Migration[6.0]
   def change
     create_table :pays do |t|
-      t.string :nom
-      t.string :code_iso
+      t.string :nom, null: false
+      t.string :code_iso, null: false
 
       t.timestamps
     end
