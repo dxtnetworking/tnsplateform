9e3582f2060b0324b2ebbaaf2d1db4d18ee7712f
Demba Siby <dembasiby@gmail.com>
2021-02-02 20:58:46 +0000

    Mise a jour migration

--- backend/db/migrate/20210202203908_create_telephones.rb
+++ backend/db/migrate/20210202203908_create_telephones.rb
@@ -1,8 +1,8 @@
 class CreateTelephones < ActiveRecord::Migration[6.0]
   def change
     create_table :telephones do |t|
-      t.boolean :numero_principal
-      t.string :numero
+      t.boolean :numero_principal, default: false
+      t.string :numero, null: false
       t.belongs_to :profile, null: false, foreign_key: true
 
       t.timestamps
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_160228) do
+ActiveRecord::Schema.define(version: 2021_02_02_203908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 2021_02_02_160228) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "telephones", force: :cascade do |t|
+    t.boolean "numero_principal", default: false
+    t.string "numero", null: false
+    t.bigint "profile_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["profile_id"], name: "index_telephones_on_profile_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
@@ -90,4 +99,5 @@ ActiveRecord::Schema.define(version: 2021_02_02_160228) do
   add_foreign_key "experiences", "profiles"
   add_foreign_key "formations", "profiles"
   add_foreign_key "profiles", "users"
+  add_foreign_key "telephones", "profiles"
 end
