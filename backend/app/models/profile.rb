fe0fc9c17ac325aaecc65be81e71d6fd2188fc0a
Demba Siby <dembasiby@gmail.com>
2021-02-02 20:56:42 +0000

    Ajout validations

--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,7 +1,9 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  
   has_many :formations
   has_many :experiences
+  has_many :telephones
   
   validates :niveau_etude, presence: true, allow_blank: false
   validates :occupation_actuelle, presence: true, allow_blank: false
--- backend/app/models/telephone.rb
+++ backend/app/models/telephone.rb
@@ -1,3 +1,5 @@
 class Telephone < ApplicationRecord
   belongs_to :profile
+  
+  validates :numero, presence: true, allow_blank: false
 end
