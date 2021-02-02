ea1594aae3ee492f69e520c01a1832142d19a25a
Demba Siby <dembasiby@gmail.com>
2021-02-02 16:14:36 +0000

    Ajout validation de base au  modele

--- backend/app/models/experience.rb
+++ backend/app/models/experience.rb
@@ -1,3 +1,11 @@
 class Experience < ApplicationRecord
   belongs_to :profile
+  
+  
+  validates :titre_poste, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
+  validates :type_poste, presence: true, allow_blank: false
+  validates :localisation, presence: true, allow_blank: false
+  validates :debut_occupation_poste, presence: true, allow_blank: false
+  
 end
--- backend/app/models/profile.rb
+++ backend/app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
   has_many :formations
+  has_many :experiences
   
   validates :niveau_etude, presence: true, allow_blank: false
   validates :occupation_actuelle, presence: true, allow_blank: false
