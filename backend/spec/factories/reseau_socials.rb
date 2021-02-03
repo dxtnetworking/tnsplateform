1e25fb61c9b412ba1ba3ca7eb62a817adb1a27ec
Demba Siby <dembasiby@gmail.com>
2021-02-03 13:33:09 +0000

    Tests standards pour reseau social

--- backend/spec/factories/reseau_socials.rb
+++ backend/spec/factories/reseau_socials.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :reseau_social do
-    nom_reseau { "MyString" }
-    url { "MyString" }
+    nom_reseau { Faker::Company.name }
+    url { Faker::Internet.url }
   end
 end
