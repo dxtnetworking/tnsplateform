e1c8955f581d0a2db03b1a5658a659d48bffc58d
Demba Siby <dembasiby@gmail.com>
2021-02-03 00:00:42 +0000

    Mise a jour migration table villes

--- backend/db/migrate/20210202234249_create_villes.rb
+++ backend/db/migrate/20210202234249_create_villes.rb
@@ -1,7 +1,7 @@
 class CreateVilles < ActiveRecord::Migration[6.0]
   def change
     create_table :villes do |t|
-      t.string :nom
+      t.string :nom, null: false
       t.belongs_to :pay, null: false, foreign_key: true
 
       t.timestamps
