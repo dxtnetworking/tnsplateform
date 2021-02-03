fdbca567e4c47edb4891ae66a0fb6b134628f0ae
Demba Siby <dembasiby@gmail.com>
2021-02-03 13:12:35 +0000

    Mise a jour migration table reseau_socials

--- backend/app/models/reseau_social.rb
+++ backend/app/models/reseau_social.rb
@@ -1,2 +1,4 @@
 class ReseauSocial < ApplicationRecord
+  validates :nom_reseau, presence: true
+  validates :url, presence: true
 end
--- backend/db/migrate/20210203130810_create_reseau_socials.rb
+++ backend/db/migrate/20210203130810_create_reseau_socials.rb
@@ -1,8 +1,8 @@
 class CreateReseauSocials < ActiveRecord::Migration[6.0]
   def change
     create_table :reseau_socials do |t|
-      t.string :nom_reseau
-      t.string :url
+      t.string :nom_reseau, null: false
+      t.string :url, null: false
 
       t.timestamps
     end
